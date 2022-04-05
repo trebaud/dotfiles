6291f1a1c286aa7a57514bd6eabf19a3b9c0ab9f
thomas <thomasr@unito.io>
2022-04-04 22:06:49 -0400

    remove deprecated install script

--- README.md
+++ README.md
@@ -1,22 +1,4 @@
 # Dotfiles
 
-Here you will find my dotfiles, use them as you please. This is intended to provide a minimalistic development environment and you will find more complete configurations elsewhere.
-
-The setup script installs the following programs:
-
-* [tmux](https://github.com/tmux/tmux)
-* [tig](https://github.com/jonas/tig)
-* [fish](https://fishshell.com/) and [oh-my-fish](https://github.com/oh-my-fish/oh-my-fish)
-* [spaceship-prompt](https://github.com/denysdovhan/spaceship-prompt)
-* [Neovim](https://github.com/neovim/neovim)
-* [Plug](https://github.com/junegunn/vim-plug)
-* [Powerline-fonts](https://github.com/powerline/fonts)
-
-Clone this repository and run the setup script. To install individual programs send in additionnal arguments with the install parameter.
-
-``` bash
-  $ ./setup.sh -i=tmux;zsh;tig
-```
-![](show.png)
-
-
+Here you will find my dotfiles, use them as you please.
+This is intended to provide a minimalistic development environment and you will find more complete configurations elsewhere.
--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,9 @@
 # import profile environment variables
 fenv source ~/.profile
 
-
 # Aliases
 alias lss='ranger'
 alias cl='clear'
-
-alias grhh='git reset HEAD --hard'
-alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 
 function current_branch
   set ref (git symbolic-ref HEAD 2> /dev/null); or \
@@ -15,17 +11,18 @@ function current_branch
   echo ref | sed s-refs/heads--
 end
 
-alias ggpull='git pull origin (current_branch)'
-alias ggpur='git pull --rebase origin (current_branch)'
-alias ggpush='git push origin (current_branch)'
-alias ggpnp='git pull origin (current_branch); and git push origin (current_branch)'
-
 function _git_log_prettily
   if ! [ -z $1 ]; then
     git log --pretty=$1
   end
 end
 
+alias ggpull='git pull origin (current_branch)'
+alias ggpur='git pull --rebase origin (current_branch)'
+alias ggpush='git push origin (current_branch)'
+alias ggpnp='git pull origin (current_branch); and git push origin (current_branch)'
+alias grhh='git reset HEAD --hard'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 alias glp="_git_log_prettily"
 
 # cURL
