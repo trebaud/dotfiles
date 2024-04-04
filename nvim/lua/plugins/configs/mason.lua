03eca941632fa40eb3414b6075998a0b2a9bfed0
thomas <rebaud.thomas@gmail.com>
2024-04-03 22:25:00 -0400

    add new nvim conf

---
+++ nvim/lua/plugins/configs/mason.lua
@@ -0,0 +1,13 @@
+return {
+  ensure_installed = {
+    "tsserver",
+    "gofumpt",
+    "goimports",
+    "golines",
+    "gopls",
+    "lua-language-server",
+    "rust-analyzer",
+    "rustfmt",
+    "stylua",
+  },
+}
